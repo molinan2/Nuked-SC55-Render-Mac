e467b2ebfd4d4f21e6937a4776b0c0b7afb25906
J.C. Moyer <jcmoyer32@gmail.com>
2025-04-02 16:41:57 -0400

    emu: implement hash-based rom loading

--- src/backend/emu.h
+++ src/backend/emu.h
@@ -60,6 +60,23 @@ enum class EMU_SystemReset {
     GM_RESET,
 };
 
+// Where a file should be mapped to once loaded
+enum class EMU_RomDestination
+{
+    ROM1,
+    ROM2,
+    WAVEROM1,
+    WAVEROM2,
+    WAVEROM3,
+    SMROM,
+
+    // do not reorder these
+    COUNT,
+    NONE = COUNT,
+};
+
+struct EMU_AllRomsetMaps;
+
 struct Emulator {
 public:
     Emulator() = default;
@@ -86,6 +103,9 @@ public:
 
     bool LoadRoms(Romset romset, const std::filesystem::path& base_path);
 
+    // pre: EMU_IsCompleteRomset(romset_maps, romset)
+    bool LoadRomsAuto(Romset romset, const EMU_AllRomsetMaps& romset_maps);
+
     void PostMIDI(uint8_t data_byte);
     void PostMIDI(std::span<const uint8_t> data);
 
@@ -101,6 +121,8 @@ private:
     void SaveNVRAM();
     void LoadNVRAM();
 
+    std::span<uint8_t> MapBuffer(EMU_RomDestination romdest);
+
 private:
     std::unique_ptr<mcu_t>       m_mcu;
     std::unique_ptr<submcu_t>    m_sm;
@@ -110,26 +132,12 @@ private:
     EMU_Options                  m_options;
 };
 
-// Where a file should be mapped to once loaded
-enum class EMU_RomDestination
-{
-    ROM1,
-    ROM2,
-    WAVEROM1,
-    WAVEROM2,
-    WAVEROM3,
-    SMROM,
-
-    // do not reorder these
-    COUNT,
-    NONE = COUNT,
-};
-
-// Maps rom destinations to filenames on disk
+// Maps rom destinations to filenames and their contents on disk
 struct EMU_RomFilenameMap
 {
     // Array indexed by EMU_RomDestination
     std::filesystem::path rom_paths[(size_t)EMU_RomDestination::COUNT]{};
+    std::vector<uint8_t>  rom_data[(size_t)EMU_RomDestination::COUNT]{};
 };
 
 // Maps romsets to filename maps
@@ -149,7 +157,11 @@ bool EMU_GetRomsets(const std::filesystem::path& base_path, EMU_AllRomsetMaps& a
 
 // Returns true if `all_maps` contains all the files required to load `romset`. Missing roms will be reported in
 // `missing`.
+bool EMU_IsCompleteRomset(const EMU_AllRomsetMaps& all_maps, Romset romset);
 bool EMU_IsCompleteRomset(const EMU_AllRomsetMaps& all_maps, Romset romset, std::vector<EMU_RomDestination>& missing);
+
+// Returns true if `destination` represents a waverom destination.
+bool EMU_IsWaverom(EMU_RomDestination destination);
 
 // Returns `destination` as a string.
 const char* EMU_RomDestinationToString(EMU_RomDestination destination);
